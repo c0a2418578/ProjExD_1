805a81310a5ca0822a54eba38ae738cf0f9e7ad8
c0a24185 <c0a2418578@edu.teu.ac.jp>
2025-11-18 17:57:13 +0900

    演習2

---
+++ flying_kokaton.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
+    bg_img2 = pg.transform.flip(bg_img, True, False)
+    kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
+    tmr = 0
+    x = 0
+    y = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+        screen.blit(bg_img2, [-tmr + 1600, 0])
+        screen.blit(bg_img, [-tmr, 0])
+        screen.blit(bg_img, [-tmr+3200, 0])
+        kk_rct = kk_img.get_rect()
+        kk_rct.center = 300, 200
+        k = pg.key.get_pressed()
+        if k[pg.K_UP]:
+            y -= 1
+        elif k[pg.K_DOWN]:
+            y += 1
+        elif k[pg.K_RIGHT]:
+            x += 2 #演習課題1の②をコミットする際に間違えて演習課題2とコミットしてしまいました。
+        elif k[pg.K_LEFT]:
+            x -= 1
+        kk_rct.move_ip((x-tmr, y))
+        screen.blit(kk_img, kk_rct)
+        pg.display.update()
+        tmr += 1
+        if tmr == 3199:
+            tmr = 0
+        clock.tick(200)
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
